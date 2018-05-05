f6879382d0b5ee87b8bb5871049173bb19a15703
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-05-05 22:40:17 +0200

    First

---
+++ src/containers/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.stste = {
+            data: []
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({data: remainder});
+    }
+
+    render() {
+        return (
+            <div className={style.TodoApp}>
+            // Tu pojawią się komponenty aplikacj
+            </div>
+        );
+    }
+}
+
+export default App;
